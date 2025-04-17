2c9269577bc357403f8bb039182be4d48c486eec
miguel Gonzalez <migue2000gw@gmail.com>
2025-04-16 21:11:59 -0500

    https://github.com/mauricioestupian/ApiPoli.git

--- lib/HomePrincipal.dart
+++ lib/HomePrincipal.dart
@@ -1,1 +1,44 @@
 import 'package:flutter/material.dart';
+
+class Homeprincipal extends
+StatefulWidget{
+  const Homeprincipal({super.key});
+
+  @override
+  State<Homeprincipal> createState() =>
+  _HomePrincipalState();
+} 
+
+class _HomePrincipalState extends
+State<Homeprincipal>{
+@override
+ Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(
+      title: Center(
+        child: const Text("Datos Personales",
+    style: TextStyle (fontWeight: FontWeight.bold, color: Colors.white),
+       ),
+       ),
+       backgroundColor: Colors.amber,
+       ),
+       body: Center(
+        
+        child: Card(
+          child: Column(
+            children: [
+              Row(children: [Text("Nombre", style: TextStyle(color: Colors.blue))]),
+              Row(children: [Text("Apellido")]),
+
+            ],
+          ) 
+        )
+       ),
+  );
+ }
+}
+
+
+
+
+
--- lib/Principal.dart
+++ lib/Principal.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:proyect/HomePrincipal.dart';
 
 class Principal extends StatelessWidget {
   const Principal ({super.key});
 
   @override
   Widget build (BuildContext context){
-    return MaterialApp(
+    return MaterialApp( 
       title: "Politecnico: Primera Clase",
       debugShowCheckedModeBanner: false,
+      home: Homeprincipal(),
     );
   }
 }
